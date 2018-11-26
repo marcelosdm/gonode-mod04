b6c0c2d622aa3f4cc867401477913bd0ead2a8ec
marcelosdm <marcelosdm@gmail.com>
2018-11-26 04:58:25 -0200

    Add Password update method

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -32,6 +33,36 @@ class ForgotPasswordController {
     } catch (err) {
       return response.status(err.status).send({
         error: { message: 'Something went wrong. Does this email exist?' }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'The reset token is expired' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({
+        error: {
+          message: 'Something went wrong. Your password has not been reset.'
+        }
       })
     }
   }
